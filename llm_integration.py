dad30acc26bbe33ce4604d4854fcd63c4101ecbc
nikki617 <nikki617@bu.edu>
2024-10-05 18:14:34 -0400

    Update llm_integration.py

--- llm_integration.py
+++ llm_integration.py
@@ -1,25 +1,23 @@
 # llm_integration.py
 
-from langchain_core.messages import HumanMessage
-from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain.agents import AgentExecutor, create_tool_calling_agent
-from langchain_community.chat_message_histories import StreamlitChatMessageHistory
+from langchain_core.prompts import ChatPromptTemplate
+from calendar_integration import list_event_tool, add_event_tool
 
-class LangChainManager:
-    def __init__(self, api_key):
-        self.llm = ChatOpenAI(api_key=api_key, temperature=0.1)
-        self.prompt = ChatPromptTemplate.from_messages(
-            [
-                ("system", "You are a helpful Google Calendar assistant"),
-                ("human", "{input}"),
-                ("placeholder", "{agent_scratchpad}"),
-            ]
-        )
+def create_agent():
+    # Create the LLM
+    llm = ChatOpenAI(api_key="YOUR_OPENAI_API_KEY", temperature=0.1)
 
-    def create_agent(self, tools):
-        agent = create_tool_calling_agent(self.llm, tools, self.prompt)
-        return AgentExecutor(agent=agent, tools=tools)
+    # Messages used by the chatbot
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", "You are a helpful Google Calendar assistant"),
+            ("human", "{input}"),
+            ("placeholder", "{agent_scratchpad}"),
+        ]
+    )
 
-    def load_message_history(self):
-        return StreamlitChatMessageHistory(key="special_app_key")
+    # Creating the agent that will integrate the provided calendar tool with the LLM.
+    agent = create_tool_calling_agent(llm, [list_event_tool, add_event_tool], prompt)
+    return AgentExecutor(agent=agent)
