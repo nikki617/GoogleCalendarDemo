4b7e77ad086ba53cb34476026fec347c832eb8b4
nikki617 <nikki617@bu.edu>
2024-10-05 20:16:47 -0400

    Update llm_integration.py

--- llm_integration.py
+++ llm_integration.py
@@ -1,11 +1,10 @@
 # llm_integration.py
 from langchain_openai import ChatOpenAI
-from langchain.agents import AgentExecutor, create_tool_calling_agent
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_community.chat_message_histories import StreamlitChatMessageHistory
+from langchain.agents import create_tool_calling_agent, AgentExecutor
 import streamlit as st
 
-def create_chatbot(llm, tools):
+def setup_llm(tools):
     # Create the LLM
     llm = ChatOpenAI(api_key=st.secrets["openai"]["api_key"], temperature=0.1)
 
@@ -20,9 +19,9 @@ def create_chatbot(llm, tools):
 
     # Create the agent
     agent = create_tool_calling_agent(llm, tools, prompt)
-    agent = AgentExecutor(agent=agent, tools=tools)
-
-    # Storing message history
-    msgs = StreamlitChatMessageHistory(key="special_app_key")
-
-    return agent, msgs
+    agent_executor = AgentExecutor(
+        agent=agent, 
+        tools=tools,
+    )
+    
+    return agent_executor
