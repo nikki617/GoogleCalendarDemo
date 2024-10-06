44bcba190e0d08eff5fbf07b0ebae0b30be682ea
nikki617 <nikki617@bu.edu>
2024-10-05 20:00:17 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -141,6 +141,6 @@ with col2:
     st.subheader("Your Calendar")
     calendar_embed_code = """
     <iframe src="https://calendar.google.com/calendar/embed?src=nikki617%40bu.edu&ctz=Europe%2FBerlin" 
-            style="border: 0" width="100%" height="650" frameborder="0" scrolling="no"></iframe>
+            style="border: 0" width="900" height="650" frameborder="0" scrolling="no"></iframe>
     """
     st.components.v1.html(calendar_embed_code, height=650)
