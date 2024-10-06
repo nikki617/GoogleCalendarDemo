2684c04f60ea80f90ffedf5e64783fa04b247335
nikki617 <nikki617@bu.edu>
2024-10-06 11:22:57 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,20 +2,9 @@
 
 import streamlit as st
 from datetime import datetime, timedelta
-from streamlit_community.chat_message_histories import StreamlitChatMessageHistory
-from streamlit_community.callbacks.streamlit import StreamlitCallbackHandler
-from langchain.callbacks.tracers import ConsoleCallbackHandler
+from langchain_community.chat_message_histories import StreamlitChatMessageHistory
 
-# Import functions from other files
-from calendar_integration import get_calendar
-from llm_integration import create_tools, create_llm_agent
-
-# Initialize the calendar
-calendar = get_calendar()
-
-# Create LangChain tools and agent
-tools = create_tools(calendar)
-agent = create_llm_agent(tools)
+from llm_integration import create_llm_agent, invoke_agent
 
 # Initialize message history
 msgs = StreamlitChatMessageHistory(key="special_app_key")
@@ -29,23 +18,22 @@ for msg in msgs.messages:
     if msg.type in ["ai", "human"]:
         st.chat_message(msg.type).write(msg.content)
 
+# Initialize the LLM agent
+agent = create_llm_agent()
+
 # When the user enters a new prompt
 if entered_prompt := st.chat_input("What does my day look like?"):
     # Add human message
     st.chat_message("human").write(entered_prompt)
     msgs.add_user_message(entered_prompt)
 
-    # Get a response from the agent
-    st_callback = StreamlitCallbackHandler(st.container())
-    
     # Specify the default date range for the current week
     from_datetime = datetime.now()
     to_datetime = datetime.now() + timedelta(days=7)
-    
-    # Invoke the agent with the entered prompt and the date range
-    response = agent.invoke({"input": entered_prompt, "from_datetime": from_datetime, "to_datetime": to_datetime}, {"callbacks": [st_callback, ConsoleCallbackHandler()]})
+
+    # Get a response from the agent
+    response = invoke_agent(agent, entered_prompt, from_datetime, to_datetime)
 
     # Add AI response
-    response = response["output"]
     st.chat_message("ai").write(response)
     msgs.add_ai_message(response)
